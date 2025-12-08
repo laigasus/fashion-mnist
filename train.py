512844d0a9df7cb49ecc9d088f78ca9061af2328
Ok JaeOok <okjaeook98@gmail.com>
2025-12-08 22:47:51 +0900

    train only logic
    Updated formatting for consistency and removed test loader.

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500)
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
@@ -27,7 +27,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        x = x.view(-1, 4 * 4 * 50)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
@@ -45,30 +45,14 @@ def train(args, model, device, train_loader, optimizer, epoch):
         if batch_idx % args.log_interval == 0:
             msg = "Train Epoch: {} [{}/{} ({:.0f}%)]\tloss={:.4f}".format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item())
+                       100. * batch_idx / len(train_loader), loss.item())
             logging.info(msg)
             niter = epoch * len(train_loader) + batch_idx
 
 
-def test(args, model, device, test_loader, epoch):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    logging.info("{{metricName: accuracy, metricValue: {:.4f}}};{{metricName: loss, metricValue: {:.4f}}}\n".format(
-        float(correct) / len(test_loader.dataset), test_loss))
-
-
 def should_distribute():
     return dist.is_available() and WORLD_SIZE > 1
+
 
 def is_distributed():
     return dist.is_available() and dist.is_initialized()
@@ -79,9 +63,7 @@ def main():
     parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
     parser.add_argument("--batch-size", type=int, default=64, metavar="N",
                         help="input batch size for training (default: 64)")
-    parser.add_argument("--test-batch-size", type=int, default=1000, metavar="N",
-                        help="input batch size for testing (default: 1000)")
-    parser.add_argument("--epochs", type=int, default=10, metavar="N",
+    parser.add_argument("--epochs", type=int, default=1, metavar="N",
                         help="number of epochs to train (default: 10)")
     parser.add_argument("--lr", type=float, default=0.01, metavar="LR",
                         help="learning rate (default: 0.01)")
@@ -141,14 +123,6 @@ def main():
                               ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    test_loader = torch.utils.data.DataLoader(
-        datasets.FashionMNIST("./data",
-                              train=False,
-                              transform=transforms.Compose([
-                                  transforms.ToTensor()
-                              ])),
-        batch_size=args.test_batch_size, shuffle=False, **kwargs)
-
     model = Net().to(device)
 
     if is_distributed():
@@ -160,7 +134,6 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader, epoch)
 
     if (args.save_model):
         torch.save(model.state_dict(), "mnist_cnn.pt")
